b1bf8c6186c5d215b3124e368d5d213f2ff9957d
BlockHero <70573427+itachi2003@users.noreply.github.com>
2023-10-06 13:43:19 +0400

    Update Halve.js

--- Halve.js
+++ Halve.js
@@ -1,1 +1,10 @@
+function halfValue(numbers) {
+    let newArray = []
+    for(let i = 0; i < numbers.length; i++) {
+        newArray[i] = Math.round(numbers[i] / 2)
+    }
+    return newArray
+}
 
+
+module.exports = halfValue;
